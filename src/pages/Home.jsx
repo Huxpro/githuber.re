d201edc32558fc2510df485070c67e1ce45a60d3
黄玄 <huxpro@gmail.com>
2017-06-30 01:21:23 +0800

    clean Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,7 @@
-import React, { Component } from 'react';
-import RECO from '../model/reco'
-import { Link } from 'react-router-dom';
+import { Component } from 'react';
 
 export default class Home extends Component {
-  state = {
-    data: RECO
-  }
-
   render() {
-    const { data } = this.state;
-    if (!data) return null;
-
-    const list = data.items.map(item => (
-      <Link to={`/${item.login}`} key={item.login}>
-        <li className="rc-list-item">
-          <img src={item.avatar_url} alt={item.login} />
-          <span className="rc-list-item__text">
-            {item.login}
-          </span>
-        </li>
-      </Link>
-    ))
-
-    return (
-      <div className="rc-list app-container">
-        <h4 className="rc-box" style={{
-          marginBottom: 0
-        }}> RECOMM - JSConfCN 2017</h4>
-        {list}
-      </div>
-    )
+    return null;
   }
 }
